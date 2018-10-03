13407f15ea0ed415e83f7beb0509e051d4a867a7
Vidal Gonzalez <darkvidal@gmail.com>
2018-10-02 20:42:58 -0500

    Setting production webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Footer from './components/footer'
-import PhotoGallery from './components/photo-gallery'
-import Header from './components/header'
-import Slider from './components/showcase-slider'
+import Footer from './components/footer';
+import PhotoGallery from './components/photo-gallery';
+import Header from './components/header';
+import Slider from './components/showcase-slider';
 
+import './styles.css';
 
-import "./styles.css";
-
-const App = () => (  
-    <div>
-      <Header />
-      <Slider />
-      <PhotoGallery />
-      <Footer />
-    </div>  
+const App = () => (
+  <div>
+    <Header />
+    <Slider />
+    <PhotoGallery />
+    <Footer />
+  </div>
 );
 
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = env => {
+  const isProduction = env === 'production';
+
+  return {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'                
-            ]
-        }]
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+      contentBase: path.join(__dirname, 'public')
     },
     plugins: [
-        new HtmlWebpackPlugin({
-          template: './public/index.html',
-          filename: './index.html',
-          favicon: './public/img/favicon.png'
-        })
-      ]
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+        favicon: './public/img/favicon.png'
+      })
+    ]
+  };
 };
