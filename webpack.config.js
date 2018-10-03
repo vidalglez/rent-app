0d4d82df0af394ea19f3232888859cf2528ef175
Vidal Gonzalez <darkvidal@gmail.com>
2018-10-02 21:24:03 -0500

    Creating separating CSS files webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = env => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/index.js',
@@ -19,11 +21,21 @@ module.exports = env => {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader']
+          use: CSSExtract.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         }
       ]
     },
-    devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public')
     },
@@ -32,7 +44,8 @@ module.exports = env => {
         template: './public/index.html',
         filename: './index.html',
         favicon: './public/img/favicon.png'
-      })
+      }),
+      CSSExtract
     ]
   };
 };
