175255ccfe142c3b7662fcd0a949fd261d705b60
Vidal Gonzalez <darkvidal@gmail.com>
2018-10-02 20:04:03 -0500

    setting initial webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'                
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: './index.html',
+          favicon: './public/img/favicon.png'
+        })
+      ]
 };
